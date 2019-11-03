d6e8af609ce31ac376c742ecf4cfef96432a1065
Piers Karsenbarg <email@piersonthe.net>
2019-11-03 21:42:55 +0000

    Updated base

--- index.ts
+++ index.ts
@@ -2,8 +2,59 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const current = aws.getCallerIdentity();
+const rootArn = `arn:aws:iam::${current.accountId}:root`;
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const role = new aws.iam.Role("assume_admin_role-pulumi",{
+    maxSessionDuration: 28800,
+    assumeRolePolicy: {
+        Version: "2012-10-17",
+        Statement: [{
+            Action: "sts:AssumeRole",
+            Principal: { "AWS": rootArn },
+            Effect: "Allow",
+            Sid: "",
+        }]
+    }
+});
+
+const policy = new aws.iam.Policy("assume_admin_policy-pulumi",
+    {
+        policy: role.arn.apply(arn => JSON.stringify({
+                Version: "2012-10-17",
+                Statement: [
+                    {
+                        Sid: "",
+                        Effect: "Allow",
+                        Action: "sts:AssumeRole",
+                        Resource: `${arn}`,
+                        Condition: {
+                            BoolIfExists: {
+                                "aws:MultiFactorAuthPresent": "true"
+                            }
+                        }
+                    }
+                ]
+            }
+        ))
+    }, {dependsOn: [role]}
+);
+
+const rolePolicyAttachment = new aws.iam.RolePolicyAttachment("admin_access_role_policy_attachment",{
+    role: role,
+    policyArn: aws.iam.ManagedPolicies.AdministratorAccess
+});
+
+const group = new aws.iam.Group("AssumeRolePulumi");
+
+const policyAttachment = new aws.iam.PolicyAttachment("admin_policy_attachment", {
+    groups: [group],
+    policyArn: policy.arn
+})
+
+const user = new aws.iam.User("piers-pulumi", {});
+
+const devTeam = new aws.iam.GroupMembership("dev-team", {
+    group: group.name,
+    users: [ user.name ] 
+});
